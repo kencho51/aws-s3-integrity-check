6092d65c4cb4a66adb3a87e0f599bc729bbc79a0
kencho <kencho.gigascience@gmail.com>
2023-06-02 10:17:32 +0800

    Download subset of test data

--- download_ega.sh
+++ download_ega.sh
@@ -1,30 +1,16 @@
 #!/usr/bin/env bash
 
-echo "*************Start download************"
-# rclone --config rclone.conf ls ega:tf-prioritizer
-# rclone --config rclone.conf sync \
-#  --progress \
-#  --log-file rclone-ukbec-sync.log \
-#  --log-level DEBUG \
-#  --checkers 8 \
-#  --transfers 4 \
-#  --drive-chunk-size 64M \
-#  --max-backlog 999999 \
-#  ega:ukbec-unaligned-fastq \
-#  data/ukbec-unaligned-fastq
-
-rclone --config rclone.conf copy \
-  --progress \
-  --log-file rclone-ukbec.log \
-  --log-level DEBUG \
-  --checkers 8 \
-  --transfers 4 \
-  --no-traverse \
-  ega:ukbec-unaligned-fastq \
-  data/ukbec-unaligned-fastq
-status=$?
-if [ $status = 0 ]; then
-  echo "*************Download completed****************"
-else
-  echo "!!!!!!!!!!!!!Download failed!!!!!!!!!!!!!!!!!!!"
-fi
+while IFS= read -r i;
+do
+  echo "****************Start download ${i}******************"
+  rclone --config rclone.conf copy \
+    --progress \
+    --log-file rclone-ukbec.log \
+    --log-level DEBUG \
+    --checkers 8 \
+    --transfers 4 \
+    --no-traverse \
+    ega:"${i}" \
+    data/ukbec-unaligned-fastq
+    echo "****************Download ${i} completed******************"
+done < ukbec-unaligned.txt
